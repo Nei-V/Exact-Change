6fe35ef70e78ab5793f49f326d07007af191e413
Ion Varsescu <19718355+Nei-V@users.noreply.github.com>
2018-04-06 19:04:04 +0300

    Final version

--- exactchange.js
+++ exactchange.js
@@ -49,7 +49,7 @@ function exactChange() {
                 if (variableChangeNeeded < 0) {
                     return "no exact change";
                 }
-                else if (variableChangeNeeded==0){
+                else if (variableChangeNeeded == 0) {
                     break;
                 }
                 else {
@@ -63,22 +63,60 @@ function exactChange() {
                             console.log("rest0", variableChangeNeeded);
                         }
                         else {
-                            change[j][1] = originalChange[j][1];
+                            change[j][1] = originalChange[j][2];
                             variableChangeNeeded = Math.round(parseFloat(((variableChangeNeeded - originalChange[j][1]) * Math.pow(10, 2)).toFixed(2))) / Math.pow(10, 2);
                             console.log("change1", change[j][1]);
                             console.log("rest1", variableChangeNeeded);
-    }
+                        }
 
 
                     };
-                    
+
                 };
 
             };
-            let result = change.filter(change => change[index][1] !== undefined);
-            return change;
-        };
+            let result = [];
+            let totalChangeToGive = 0;
+            
+            for (let x = 8; x > -1; x--) {
+                console.log(x);
+                console.log("number of coins", change[x][1]);
+                if (change[x][1] !== undefined) {
+                    change[x][1] = change[x][1] * valuesOfChangeArray[x][1];
+                    console.log("value of coins", change[x][1]);
+                    console.log("value of change", valuesOfChangeArray[x][1]);
+                };
+            };
+            //maybe the if can also be inserted in the filter function, thus not needing the for loop.
+            result = change.filter((x) => {
+                if (x[1] !== undefined) {
+                    totalChangeToGive = totalChangeToGive + x[1];
+                    return x[1];
+                };
+                console.log("totalChangeTogive", totalChangeToGive);
+            }).reverse();
 
+            
+
+            /* same thing with if instead of filter - seems shorter because I can use the counter inside
+            also, some say that in the filter arrow function "Doing the counting in the filter callback is somewhat... not so stylish.
+             I really like pure functions (functions that return the same values on same input all the time and have no side-effects)"
+
+                            if (change[x][1] !== undefined) {
+                                result.push(change[x]);
+                                totalChangeToGive = totalChangeToGive + change[x][1];
+                            };
+                            */
+
+
+
+            if (totalChangeToGive < neededChange) {
+                return "Insufficient Funds";//means that there is no exact change - this is the wording in the requirement
+            }
+            else {
+                return result;
+            };
+        };
     };
 
 
@@ -97,352 +135,6 @@ function exactChange() {
 
 
 
-    document.getElementById("result").innerHTML = checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
+    document.getElementById("result").innerHTML = checkCashRegister(3.26, 100.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
 
 };
-
-/*
-for (let j = 8; j > -1; j--) {
-    if (variableChangeNeeded < 0) {
-        return "no exact change";
-    }
-    else {
-        if (variableChangeNeeded > 100) {
-
-            if (variableChangeNeeded <= originalChange[j][1]) {
-                change[j][1] = Math.floor(variableChangeNeeded / valuesOfChangeArray[j][1]);
-                console.log("change", change[j][1]);
-                variableChangeNeeded = variableChangeNeeded % (change[j][1] * valuesOfChangeArray[j][1]);
-                console.log("rest", variableChangeNeeded);
-            }
-            else {
-                change[j][1] = originalChange[j][1];
-                variableChangeNeeded = variableChangeNeeded - originalChange[j][1];
-                console.log("change", change[j][1]);
-                console.log("rest", variableChangeNeeded);
-            }
-
-        }
-        else if (variableChangeNeeded > 20) {
-            console.log(j);
-
-            if (variableChangeNeeded <= originalChange[j][1]) {
-                change[j][1] = Math.floor(variableChangeNeeded / valuesOfChangeArray[j][1]);
-                console.log("change", change[j][1]);
-                variableChangeNeeded = variableChangeNeeded % (change[j][1] * 20);
-                console.log("rest", variableChangeNeeded);
-            }
-            else {
-                change[j][1] = originalChange[j][1];
-                variableChangeNeeded = variableChangeNeeded - originalChange[j][1];
-                console.log("change", change[j][1]);
-                console.log("rest", variableChangeNeeded);
-            }
-
-        }
-        else if (variableChangeNeeded > 10) {
-
-            if (variableChangeNeeded <= originalChange[j][1]) {
-                change[j][1] = Math.floor(variableChangeNeeded / valuesOfChangeArray[j][1]);
-                console.log("change", change[j][1]);
-                variableChangeNeeded = variableChangeNeeded % (change[j][1] * 10);
-                console.log("rest", variableChangeNeeded);
-            }
-            else {
-                change[j][1] = originalChange[j][1];
-                variableChangeNeeded = variableChangeNeeded - originalChange[j][1];
-                console.log("change", change[j][1]);
-                console.log("rest", variableChangeNeeded);
-            }
-
-        }
-        else if (variableChangeNeeded > 5) {
-
-            if (variableChangeNeeded <= originalChange[j][1]) {
-                change[j][1] = Math.floor(variableChangeNeeded / valuesOfChangeArray[j][1]);
-                console.log("change", change[j][1]);
-                variableChangeNeeded = variableChangeNeeded % (change[j][1] * 5);
-                console.log("rest", variableChangeNeeded);
-            }
-            else {
-                change[j][1] = originalChange[j][1];
-                variableChangeNeeded = variableChangeNeeded - originalChange[j][1];
-                console.log("change", change[j][1]);
-                console.log("rest", variableChangeNeeded);
-            }
-
-        }
-        else if (variableChangeNeeded > 1) {
-
-            if (variableChangeNeeded <= originalChange[j][1]) {
-                change[j][1] = Math.floor(variableChangeNeeded / valuesOfChangeArray[j][1]);
-                console.log("change", change[j][1]);
-                variableChangeNeeded = variableChangeNeeded % (change[j][1] * 1);
-                console.log("rest", variableChangeNeeded);
-            }
-            else {
-                change[j][1] = originalChange[j][1];
-                variableChangeNeeded = variableChangeNeeded - originalChange[j][1];
-                console.log("change", change[j][1]);
-                console.log("rest", variableChangeNeeded);
-            }
-
-        }
-        else if (variableChangeNeeded > 0.25) {
-
-            if (variableChangeNeeded <= originalChange[j][1]) {
-                change[j][1] = Math.floor(variableChangeNeeded / valuesOfChangeArray[j][1]);
-                console.log("change", change[j][1]);
-                variableChangeNeeded = variableChangeNeeded % (change[j][1] * 0.25);
-                console.log("rest", variableChangeNeeded);
-            }
-            else {
-                change[j][1] = originalChange[j][1];
-                variableChangeNeeded = variableChangeNeeded - originalChange[j][1];
-                console.log("change", change[j][1]);
-                console.log("rest", variableChangeNeeded);
-            }
-
-}
-            originalChange[i].push(Math.round(originalChange[i][1] / valuesOfChangeArray[i][1]));
-            console.log(originalChange[i]);
-            totalChange = totalChange + originalChange[i][1];
-            console.log("total change", totalChange);
-        };
-        console.log("final change at start", change);
-
-        if (neededChange > totalChange) {
-            return "Insufficient Funds";
-        }
-        else if (neededChange === totalChange) {
-            return "Closed";
-        }
-        else if (neededChange == 0) {
-            return "no need for change";
-        }
-        else if (neededChange < 0) {
-            return "not enough money";
-        }
-        else {
-            let variableChangeNeeded = neededChange;
-            for (let j = 8; j > -1; j--) {
-                if (variableChangeNeeded < 0) {
-                    return "no exact change";
-                }
-                else if (variableChangeNeeded==0){
-                    break;
-                }
-                else {
-                    if (variableChangeNeeded >= valuesOfChangeArray[j][1]) {
-
-                        if (variableChangeNeeded <= originalChange[j][1]) {
-                            change[j][1] = (Math.floor(variableChangeNeeded / valuesOfChangeArray[j][1]));
-                            console.log("change0", change[j][1]);
-                            //Math.round(parseFloat(((variableChangeNeeded - originalChange[j][1]) * Math.pow(10, 2)).toFixed(2))) / Math.pow(10, 2);
-                            variableChangeNeeded = Math.round(parseFloat(((variableChangeNeeded % (change[j][1] * valuesOfChangeArray[j][1])) * Math.pow(10, 2)).toFixed(2))) / Math.pow(10, 2);
-                            console.log("rest0", variableChangeNeeded);
-                        }
-                        else {
-                            change[j][1] = originalChange[j][1];
-                            variableChangeNeeded = Math.round(parseFloat(((variableChangeNeeded - originalChange[j][1]) * Math.pow(10, 2)).toFixed(2))) / Math.pow(10, 2);
-                            console.log("change1", change[j][1]);
-                            console.log("rest1", variableChangeNeeded);
-                        }
-
-
-                    };
-                    
-                };
-
-            };
-            let result = change.filter(change => change[index][1] !== undefined);
-            return change;
-        };
-
-    };
-
-
-
-
-    // Example cash-in-drawer array:
-    // [["PENNY", 1.01],
-    // ["NICKEL", 2.05],
-    // ["DIME", 3.10],
-    // ["QUARTER", 4.25],
-    // ["ONE", 90.00],
-    // ["FIVE", 55.00],
-    // ["TEN", 20.00],
-    // ["TWENTY", 60.00],
-    // ["ONE HUNDRED", 100.00]]
-
-
-
-    document.getElementById("result").innerHTML = checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
-
-};
-
-/*
-for (let j = 8; j > -1; j--) {
-    if (variableChangeNeeded < 0) {
-        return "no exact change";
-    }
-    else {
-        if (variableChangeNeeded > 100) {
-
-            if (variableChangeNeeded <= originalChange[j][1]) {
-                change[j][1] = Math.floor(variableChangeNeeded / valuesOfChangeArray[j][1]);
-                console.log("change", change[j][1]);
-                variableChangeNeeded = variableChangeNeeded % (change[j][1] * valuesOfChangeArray[j][1]);
-                console.log("rest", variableChangeNeeded);
-            }
-            else {
-                change[j][1] = originalChange[j][1];
-                variableChangeNeeded = variableChangeNeeded - originalChange[j][1];
-                console.log("change", change[j][1]);
-                console.log("rest", variableChangeNeeded);
-            }
-
-        }
-        else if (variableChangeNeeded > 20) {
-            console.log(j);
-
-            if (variableChangeNeeded <= originalChange[j][1]) {
-                change[j][1] = Math.floor(variableChangeNeeded / valuesOfChangeArray[j][1]);
-                console.log("change", change[j][1]);
-                variableChangeNeeded = variableChangeNeeded % (change[j][1] * 20);
-                console.log("rest", variableChangeNeeded);
-            }
-            else {
-                change[j][1] = originalChange[j][1];
-                variableChangeNeeded = variableChangeNeeded - originalChange[j][1];
-                console.log("change", change[j][1]);
-                console.log("rest", variableChangeNeeded);
-            }
-
-        }
-        else if (variableChangeNeeded > 10) {
-
-            if (variableChangeNeeded <= originalChange[j][1]) {
-                change[j][1] = Math.floor(variableChangeNeeded / valuesOfChangeArray[j][1]);
-                console.log("change", change[j][1]);
-                variableChangeNeeded = variableChangeNeeded % (change[j][1] * 10);
-                console.log("rest", variableChangeNeeded);
-            }
-            else {
-                change[j][1] = originalChange[j][1];
-                variableChangeNeeded = variableChangeNeeded - originalChange[j][1];
-                console.log("change", change[j][1]);
-                console.log("rest", variableChangeNeeded);
-            }
-
-        }
-        else if (variableChangeNeeded > 5) {
-
-            if (variableChangeNeeded <= originalChange[j][1]) {
-                change[j][1] = Math.floor(variableChangeNeeded / valuesOfChangeArray[j][1]);
-                console.log("change", change[j][1]);
-                variableChangeNeeded = variableChangeNeeded % (change[j][1] * 5);
-                console.log("rest", variableChangeNeeded);
-            }
-            else {
-                change[j][1] = originalChange[j][1];
-                variableChangeNeeded = variableChangeNeeded - originalChange[j][1];
-                console.log("change", change[j][1]);
-                console.log("rest", variableChangeNeeded);
-            }
-
-        }
-        else if (variableChangeNeeded > 1) {
-
-            if (variableChangeNeeded <= originalChange[j][1]) {
-                change[j][1] = Math.floor(variableChangeNeeded / valuesOfChangeArray[j][1]);
-                console.log("change", change[j][1]);
-                variableChangeNeeded = variableChangeNeeded % (change[j][1] * 1);
-                console.log("rest", variableChangeNeeded);
-            }
-            else {
-                change[j][1] = originalChange[j][1];
-                variableChangeNeeded = variableChangeNeeded - originalChange[j][1];
-                console.log("change", change[j][1]);
-                console.log("rest", variableChangeNeeded);
-            }
-
-        }
-        else if (variableChangeNeeded > 0.25) {
-
-            if (variableChangeNeeded <= originalChange[j][1]) {
-                change[j][1] = Math.floor(variableChangeNeeded / valuesOfChangeArray[j][1]);
-                console.log("change", change[j][1]);
-                variableChangeNeeded = variableChangeNeeded % (change[j][1] * 0.25);
-                console.log("rest", variableChangeNeeded);
-            }
-            else {
-                change[j][1] = originalChange[j][1];
-                variableChangeNeeded = variableChangeNeeded - originalChange[j][1];
-                console.log("change", change[j][1]);
-                console.log("rest", variableChangeNeeded);
-            }
-
-        }
-        else if (variableChangeNeeded > 0.1) {
-
-            if (variableChangeNeeded <= originalChange[j][1]) {
-                change[j][1] = Math.floor(variableChangeNeeded / valuesOfChangeArray[j][1]);
-                console.log("change", change[j][1]);
-                variableChangeNeeded = variableChangeNeeded % (change[j][1] * 0.1);
-                console.log("rest", variableChangeNeeded);
-            }
-            else {
-                change[j][1] = originalChange[j][1];
-                variableChangeNeeded = variableChangeNeeded - originalChange[j][1];
-                console.log("change", change[j][1]);
-                console.log("rest", variableChangeNeeded);
-            }
-
-        }
-        else if (variableChangeNeeded > 0.05) {
-
-            if (variableChangeNeeded <= originalChange[j][1]) {
-                console.log("rest1", variableChangeNeeded);
-                console.log("change", originalChange[j][1]);
-                change[j][1] = Math.floor(variableChangeNeeded / valuesOfChangeArray[j][1]);
-                console.log("change", change[j][1]);
-                variableChangeNeeded = variableChangeNeeded % (change[j][1] * 0.05);
-                console.log("rest", variableChangeNeeded);
-            }
-            else {
-                console.log("rest2", variableChangeNeeded);
-                change[j][1] = originalChange[j][1];
-                variableChangeNeeded = variableChangeNeeded - originalChange[j][1];
-                console.log("change", change[j][1]);
-                console.log("rest", variableChangeNeeded);
-            }
-
-        }
-        else if (variableChangeNeeded >= 0.01) {
-
-            if (variableChangeNeeded <= originalChange[j][1]) {
-                change[j][1] = Math.floor(variableChangeNeeded / valuesOfChangeArray[j][1]);
-                console.log("change", change[j][1]);
-                variableChangeNeeded = 0;
-                console.log("rest", variableChangeNeeded);
-            }
-            else {
-                return "no exact change";
-            };
-
-        }
-        else {
-
-
-            console.log("change", change[j][1]);
-            console.log("rest", variableChangeNeeded);
-
-        }
-        //let result=change.filter(change=>change[1]!==undefined);
-        return change;
-
-    };
-
-};
-};*/
